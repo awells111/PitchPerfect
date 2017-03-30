b65e37eb17bc2acc2d3d19d0e618ea83ac665065
awells111 <awells111@gmail.com>
2017-03-29 21:18:53 -0400

    App Icons Added

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -12,51 +12,59 @@ import AVFoundation
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     var audioRecorder: AVAudioRecorder!
+    let recordingName = "recordedVoice.wav"
+    let recordingInProgress: String = "Recording in Progress"
+    let tapToRecord: String = "Tap to Record"
     
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        stopRecordingButton.isEnabled = false
         setAspectRatio()
     }
 
-    @IBAction func recordAudio(_ sender: Any) {
-        toggleRecording()
-        
-        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
-        let recordingName = "recordedVoice.wav"
-        let pathArray = [dirPath, recordingName]
-        let filePath = URL(string: pathArray.joined(separator: "/"))
-        print(filePath as Any)
-        
-        let session = AVAudioSession.sharedInstance()
-        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
-        
-        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
-        audioRecorder.delegate = self
-        audioRecorder.isMeteringEnabled = true
-        audioRecorder.prepareToRecord()
-        audioRecorder.record()
-    }
+    @IBAction func recordAudio(_ sender: UIButton) {
+        if(sender.tag == recordButton.tag) {
+            setState(recordButtonState: false, labelText: recordingInProgress)
+            
+            let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+            let pathArray = [dirPath, recordingName]
+            let filePath = URL(string: pathArray.joined(separator: "/"))
+            print(filePath as Any)
+            
+            let session = AVAudioSession.sharedInstance()
+            try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+            
+            try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+            audioRecorder.delegate = self
+            audioRecorder.isMeteringEnabled = true
+            audioRecorder.prepareToRecord()
+            audioRecorder.record()
+        } else if (sender.tag == stopRecordingButton.tag){
+            setState(recordButtonState: true, labelText: tapToRecord)
 
-    @IBAction func stopRecording(_ sender: Any) {
-        toggleRecording()
-        audioRecorder.stop()
-        let audioSession = AVAudioSession.sharedInstance()
-        try! audioSession.setActive(false)
+            audioRecorder.stop()
+            let audioSession = AVAudioSession.sharedInstance()
+            try! audioSession.setActive(false)
+        }
     }
     
+    func setState(recordButtonState:Bool, labelText:String) {
+        recordButton.isEnabled = recordButtonState
+        stopRecordingButton.isEnabled = !recordButtonState
+        recordingLabel.text = labelText
+    }
+
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         if flag {
-        performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
         } else {
             print("recording was not successful")
         }
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "stopRecording" {
             let playSoundsVC = segue.destination as! PlaySoundsViewController
@@ -68,26 +76,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     func setAspectRatio() {
         recordButton.imageView?.contentMode = .scaleAspectFit
         stopRecordingButton.imageView?.contentMode = .scaleAspectFit
-    }
-    
-    func toggleRecording() {
-        if (stopRecordingButton.isEnabled) {
-            enableRecording()
-        } else if (recordButton.isEnabled) {
-            disableRecording()
-        }
-    }
-    
-    func enableRecording() {
-        recordingLabel.text = "Tap to Record"
-        recordButton.isEnabled = true
-        stopRecordingButton.isEnabled = false
-    }
-    
-    func disableRecording() {
-        recordingLabel.text = "Recording in progress"
-        stopRecordingButton.isEnabled = true
-        recordButton.isEnabled = false
     }
     
 }
